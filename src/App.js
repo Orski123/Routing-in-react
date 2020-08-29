4ea209905eb974d42e0c6ef010b46999b2c5f86e
Orpa <orski_76@yahoo.com>
2020-08-29 15:56:27 -0400

    adding missing Routes, navigation, ..

--- src/App.js
+++ src/App.js
@@ -1,29 +1,69 @@
 import React from 'react';
-import {  Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
-import Welcome from"./components/welcome/Welcome"
+import Welcome from "./components/welcome/Welcome"
 import Error from "./components/error/Error"
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
 
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* render the Navigation component */}
+//         <Navigation />
+//         <Route
+//           exact
+//           path="/"
+//           render={(props) => <Welcome {...props} name="eric" />}
+//         />
+//         <Route path="/clock" component={Clock} />
+//         <Route path="/contact" component={Contact} />
+//       </div>
+//     );
+//   }
+// }
+// export default App;
 
 
 function App() {
   return (
     <div className="App">
-       
-        <Switch>
-            <Route  
-            path="/welcome/:name"
-            render={(props)=> <Welcome {...props} name=""/>}
-           
-             />
+
+      <Navigation />
+
+      <Switch>
+
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Orpa" />}
+        />
+
+        <Route
+          path="/Welcome/:orpadata"
+          render={(props) => <Welcome {...props} name="Orpa" />}
+        />
+
+        <Route
+          path="/clock"
+          component={Clock}
+        />
+
+        <Route
+          path="/contact"
+          component={Contact}
+        />
+
+        <Route component={Error} />
         
-            <Route component={Error} />
+      </Switch>
 
-        </Switch>
 
-       
     </div>
   );
 }
